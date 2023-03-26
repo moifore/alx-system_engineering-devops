eeaba9c4670dfd13f70d48a8a046c30b1a0e62d4
moifore <moiforejallah@gmail.com>
2023-03-26 05:08:08 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Install the puppet-lint package
 exec { 'install python packages':
-     command   => 'pip3 install flask -v 2.1.0',
-     path => ['/usr/bin/'],
-  }
+     command => '/usr/bin/ap-get -y install flask -v 2.1.0',
+}
